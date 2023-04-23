a09f22f9191c2694328a0e39feea97b19d35db5c
Christos Lytras <christos.lytras@gmail.com>
2023-04-23 12:38:19 +0300

    [FIX-DEPRECATIONS] Rename react unsafe class effects

--- src/libraries/TransformableImage/index.js
+++ src/libraries/TransformableImage/index.js
@@ -52,7 +52,7 @@ export default class TransformableImage extends PureComponent {
         };
     }
 
-    componentWillMount () {
+    UNSAFE_componentWillMount () {
         if (!this.state.imageDimensions) {
             this.getImageSize(this.props.image);
         }
@@ -62,7 +62,7 @@ export default class TransformableImage extends PureComponent {
         this._mounted = true;
     }
 
-    componentWillReceiveProps (nextProps) {
+    UNSAFE_componentWillReceiveProps (nextProps) {
         if (!sameImage(this.props.image, nextProps.image)) {
             // image source changed, clear last image's imageDimensions info if any
             this.setState({ imageDimensions: nextProps.image.dimensions, keyAcumulator: this.state.keyAcumulator + 1 });
