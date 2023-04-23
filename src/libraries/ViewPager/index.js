065e7819b3502f157093adb696c0ae3105411098
Christos Lytras <christos.lytras@gmail.com>
2023-04-23 19:21:15 +0300

    feat: enable animation for scrollToPage

--- src/libraries/ViewPager/index.js
+++ src/libraries/ViewPager/index.js
@@ -228,7 +228,7 @@ export default class ViewPager extends PureComponent {
         if (immediate) {
             InteractionManager.runAfterInteractions(() => {
                 this.scroller.startScroll(this.scroller.getCurrX(), 0, finalX - this.scroller.getCurrX(), 0, 0);
-                this.innerFlatListRef.current?.scrollToOffset({offset: finalX, animated: false});
+                this.innerFlatListRef.current?.scrollToOffset({ offset: finalX, animated: true });
                 this.innerFlatListRef.current?.recordInteraction();
             });
         } else {
